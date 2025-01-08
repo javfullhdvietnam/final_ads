a1a39b07d0296c30dd2ccc35c256fb3ffa9c0988
Dat09 <datbt.ba12-042@st.usth.edu.vn>
2025-01-08 14:06:46 +0700

    d

--- ex1.c
+++ ex1.c
@@ -4,23 +4,25 @@
 //propose an algorithm using interation and implement it in C to complete the task
 //propose and implement an alternative algorithm using recursion for this task 
 //calculate the complexity of the two approaches.justify your answer by making comment on the code
-int pow(int n, int m){
-    int result = 1;
-    for(int i = 0; i < 5; i++){
-        result *= 10;
-    }
-    return result;
-}
-int swap(int n){
-    int max = n;
-    for(int i = 0; i < 5; i++){
-        for(int j = i+1; j < 5; j++){
-           int a = n%10;
-           
-    }
-    return max;
-}
+int swap(int n);
 int main(){
     int n = 51229;
     printf("The largest possible value is %d", swap(n));
+}
+
+int swap(int n){
+    int max = n;
+    int i = 0;
+    while(i<5){
+        int j = i+1;
+        while(j<5){
+            int m = swapDigit(n,i,j);
+            if(m>max){
+                max = m;
+            }
+            j++;
+        }
+        i++;
+    }
+    return max;
 }
