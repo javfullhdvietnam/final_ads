90d145eb921453a3fdcbc602a268eea1ed01031a
Dat09 <datbt.ba12-042@st.usth.edu.vn>
2025-01-08 13:42:05 +0700

    d

--- ex1.c
+++ ex1.c
@@ -3,7 +3,11 @@
 //51229 -> impossible largest value is 92251 aftet swap 
 //propose an algorithm using interation and implement it in C to complete the task
 //propose and implement an alternative algorithm using recursion for this task 
-//calculate the complexity of the two approaches.justify your answer by making comment on the code
+//calculate the complexity of the two approaches.justify your answer by making comment on the 
+
+int swap(int n){
+
+}
 int main(){
     int n = 51229;
 
