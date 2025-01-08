3da6db6558c8a35dae12c33d6297782e6ddc5464
Dat09 <datbt.ba12-042@st.usth.edu.vn>
2025-01-08 14:32:09 +0700

    d

---
+++ txt.c
@@ -0,0 +1,5 @@
+finding a digit and swap its position in the number to obtain the largest possible value
+51229 -> impossible largest value is 92251 aftet swap 
+propose an algorithm using interation and implement it in C to complete the task
+propose and implement an alternative algorithm using recursion for this task 
+calculate the complexity of the two approaches.justify your answer by making comment on the code
